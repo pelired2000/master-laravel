c55b911ec515f97710bd8fa1e19e03c303162458
pelired2000 <pelired2000@gmail.com>
2019-12-28 23:11:43 +0100

    Instalación y rutas

---
+++ resources/views/mostrar-fecha.blade.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Fecha actual</h1>";
+echo date('d-m-Y');
+echo "<br>";
+echo "<a href='/'>Inicio</a>";
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/mostrar-fecha', function () {
+    $titulo = "Fecha del Sistema";
+    return view('mostrar-fecha',array(
+        'titulo' => $titulo
+    ));
+});
+
+/* Route::get('/pelicula/{titulo}', function ($titulo) {
+    return view('pelicula',array(
+        'titulo' => $titulo
+    ));
+});
+
+Route::get('/pelicula/{titulo?}', function ($titulo="No hay una película seleccionada :(") {
+    return view('pelicula',array(
+        'titulo' => $titulo
+    ));
+}); */
+
+Route::get('/pelicula/{titulo?}/{year?}', function ($titulo='No hay una película seleccionada :(',$year=2020) {
+    return view('pelicula',array(
+        'titulo' => $titulo,
+        'year'   => $year
+    ));
+})->where(array(
+        'titulo' => '[a-zA-Z]+',
+        'year'   => '[0-9]+'
+));
